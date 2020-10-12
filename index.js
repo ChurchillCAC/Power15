ddbe01fac04a159ad15b3f89e0e2baa22e01e8c5
Arihant Bapna <greatbapna@gmail.com>
2020-10-12 07:29:24 -0400

    Create user code

--- index.js
+++ index.js
@@ -30,6 +30,38 @@ app.get("/users", (req, res, next) => {
     });
 });
 
+app.post("/api/user/", (req, res, next) => {
+  var errors=[]
+  if (!req.body.password){
+      errors.push("No password specified");
+  }
+  if (!req.body.email){
+      errors.push("No email specified");
+  }
+  if (errors.length){
+      res.status(400).json({"error":errors.join(",")});
+      return;
+  }
+  var data = {
+      name: req.body.name,
+      email: req.body.email,
+      password : md5(req.body.password)
+  }
+  var sql ='INSERT INTO user (username, email, password) VALUES (?,?,?)'
+  var params =[data.name, data.email, data.password]
+  db.run(sql, params, function (err, result) {
+      if (err){
+          res.status(400).json({"error": err.message})
+          return;
+      }
+      res.json({
+          "message": "success",
+          "data": data,
+          "id" : this.lastID
+      })
+  });
+});
+
 app.listen(port, () => {
   console.log(`running at port ${port}`);
 });
